9646efcc0a00eb87b222841673858c8c8e278b81
tsuruoka <tsuruoka@exspice.local>
2011-07-05 16:15:50 +0900

    bmp muzukasii

--- bmp.py
+++ bmp.py
@@ -1,7 +1,20 @@
 class BMP:
-    def __init__:
+    def __init__(self):
         self.width = 0
         self.height = 0
-        self.default = 0*[["r":0,"g":0,"b":0]]
+        self.data = 0*[{"r":0,"g":0,"b":0}]
 
-    def newImage(width,height,default)
+    def newImage(self,width,height,data): #data = rgb dictionary
+        self.width = width
+        self.height = height
+        self.data = data
+
+    def read(self,filename):
+        print "open %(filename)s" % locals()
+        f = open(filename,"rb")
+        for line in f:
+            print line
+        print "done"
+
+bmp = BMP()
+bmp.read("baboon.bmp")
